cfb90bd43d4092bff3ff5ebd2208e8c253c10a63
ASPCartman <aspcartman@gmail.com>
2018-02-01 09:57:03 +0300

    Fix import

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"github.com/aspcartman/eros/storage"
-	"github.com/aspcartman/pcache"
 )
 
 func main() {
 	db := storage.NewDB("localhost", 7771, "postgres", "", "eros")
 	db.Upgrade()
-
-
 
 }
